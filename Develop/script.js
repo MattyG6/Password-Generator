d9a426ca3c3dfead29dcd12b7d64139f265c23c0
MithrandirG6 <mathewjgracie@gmail.com>
2022-11-07 14:19:42 -0400

    userInput window prompt added, if statements creating alert for invalid number or invalid password length created

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(ThisIsPasswordLength) {
+  var userInput = window.prompt("Give a number value between 8 and 128 for password length")
+  
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("That's not a valid number")
+    return
+  }
+
+  if (passwordLength < 8 || passwordLength > 128 ) {
+    window.alert("Password length must be between 8 and 128 characters")
+    return
+  }
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
